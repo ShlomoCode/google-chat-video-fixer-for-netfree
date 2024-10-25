ea9b6cbae334043520a39099f00002ba95225d52
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-10-25 03:44:04 +0300

    chore: remove all unnecessary files

--- src-bex/background.ts
+++
@@ -1,9 +0,0 @@
-import { bexBackground } from 'quasar/wrappers';
-import networkRules from './network-rules';
-
-chrome.declarativeNetRequest.updateDynamicRules({
-  removeRuleIds: networkRules.map((rule) => rule.id), // remove existing rules
-  addRules: networkRules,
-});
-
-export default bexBackground(() => {});
