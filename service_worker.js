e729e27906cdfc80ef6c322a283e73f656b3163c
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-10-28 19:53:49 +0200

    feat: enable inline view even for main frame

--- service_worker.js
+++ service_worker.js
@@ -37,7 +37,7 @@ const rules = [
         },
         condition: {
             urlFilter: 'https://chat.usercontent.google.com/download',
-            resourceTypes: [chrome.declarativeNetRequest.ResourceType.MEDIA],
+            resourceTypes: [chrome.declarativeNetRequest.ResourceType.MEDIA, chrome.declarativeNetRequest.ResourceType.MAIN_FRAME],
         },
     },
 ];
