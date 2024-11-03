337d3f63d75e679d906178c46e0af93424e9957d
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-11-03 02:18:34 +0200

    chore: remove unnecessary code

--- player.js
+++ player.js
@@ -1,20 +1,11 @@
 const videoUrl = window.location.search.replace('?url=', '');
 const videoPlayer = document.getElementById('videoPlayer');
-const loadingSpinner = document.querySelector('.loading-spinner');
 const downloadButton = document.querySelector('.download-button');
 const downloadMessage = document.querySelector('.download-message');
 
 if (videoUrl) {
     videoPlayer.src = videoUrl;
 }
-
-videoPlayer.addEventListener('loadstart', () => {
-    loadingSpinner.style.display = 'block';
-});
-
-videoPlayer.addEventListener('canplay', () => {
-    loadingSpinner.style.display = 'none';
-});
 
 downloadButton.addEventListener('click', () => {
     videoPlayer.pause();
